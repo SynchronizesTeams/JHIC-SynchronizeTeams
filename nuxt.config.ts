310627d7ae244011b6436c03616826a1049ffa08
Fir3fliesss <gheraldymoses5@gmail.com>
2025-11-08 20:50:14 +0700

    gasken

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
 
   vite: {
