5c57aa20cde8b46e479b39fbb7d0a804e26c7ac3
Fir3fliesss <gheraldymoses5@gmail.com>
2025-12-26 12:38:53 +0700

    Fix footer

--- app/app.vue
+++ app/app.vue
@@ -11,4 +11,28 @@
 </template>
 
 <script setup lang="ts">
+onMounted(() => {
+  console.log(
+      `%c
+      _________                   .__                        .__            ___________                           
+     /   _____/__.__. ____   ____ |  |_________  ____   ____ |__|_______ ___\__    ___/___ _____    _____   ______
+     \_____  <   |  |/    \_/ ___\|  |  \_  __ \/  _ \ /    \|  \___   // __ \|    |_/ __ \\__  \  /     \ /  ___/
+     /        \___  |   |  \  \___|   Y  \  | \(  <_> )   |  \  |/    /\  ___/|    |\  ___/ / __ \|  Y Y  \\___ \ 
+    /_______  / ____|___|  /\___  >___|  /__|   \____/|___|  /__/_____ \\___  >____| \___  >____  /__|_|  /____  >
+            \/\/         \/     \/     \/                  \/         \/    \/           \/     \/      \/     \/
+      
+      SynchronizeTeams`,
+      "color:#00e5ff; font-family:monospace; font-size:12px;"
+    )
+  
+    console.log(
+      "%cCreated by SynchronizeTeams",
+      "font-size:16px; font-weight:bold; color:#4CAF50;"
+    )
+  
+    console.log(
+      "%cVisit us → https://synchronizeteams.my.id",
+      "font-size: 13px; color: #2196F3; text-decoration: underline; cursor: pointer;"
+    )
+})
 </script>
